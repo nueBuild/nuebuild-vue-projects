8b0dd6bf6f635e57b47a912f247902197264bf86
Jason Witt <info@nuebuild.com>
2018-12-01 20:09:04 -0500

    Removed clever code

--- templates/vue.config.js
+++ templates/vue.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
 const argv = require('yargs').argv
 const pkgConfig = require('./package.json')
-const appRoutes = require('./src/router/appRoutes.js')
 
 const config = {
   css: {
@@ -28,19 +27,14 @@ const config = {
 }
 
 if (argv.preprocess) {
-  const routePaths = []
-  appRoutes.forEach(key => {
-    routePaths.push(key.path)
-  })
   config.configureWebpack.plugins.push(
     new PrerenderSpaPlugin({
       staticDir: path.resolve(__dirname, './dist'),
-      routes: routePaths,
+      routes: ['/'],
       postProcessHtml: function(context) {
-        var titles = {}
-        appRoutes.forEach(key => {
-          titles[key.path] = key.title
-        })
+        var titles = {
+          '/': 'My home page',
+        }
         return context.html.replace(/<title>[^<]*<\/title>/i, '<title>' + titles[context.route] + '</title>')
       },
     })
